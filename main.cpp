4958fc6b44a41c576b805f2825508ef61daafddb
I0NI <sandritamendez56@gmail.com>
2023-03-11 10:57:55 -0600

    Primer Parcial Ioni Alessandro Rodas Chavarria

---
+++ main.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+using namespace std;
+/*
+int main() {
+    int num;
+    cout << "Ingrese el numero de tabla: "; cin>>num;
+
+    while((num<1)||(num>10));
+
+    for (int x= 1;x<=10;x++){
+        cout<<num<<" * "<<x<<" = "<<num*x<<endl;
+
+    }
+    return 0;
+}
+ */
+/*
+int main(){
+    int n1=5,n2=8,n3=9;
+
+    while((n1<1)||(n1>10));
+    cout<<"\nTabla del 5\n\n";
+    for (int x= 1;x<=10;x++){
+        cout<<n1<<" * "<<x<<" = "<<n1*x<<endl;
+
+    }
+    while((n2<1)||(n2>10));
+    cout<<"\nTabla del 8\n\n";
+    for (int x= 1;x<=10;x++){
+        cout<<n2<<" * "<<x<<" = "<<n2*x<<endl;
+
+    }
+    while((n3<1)||(n3>10));
+    cout<<"\nTabla del 9\n\n";
+    for (int x= 1;x<=10;x++){
+        cout<<n3<<" * "<<x<<" = "<<n3*x<<endl;
+
+    }
+    return 0;
+}
+ */
+/*
+int main(){
+    int dia;
+    cout<<"Escriba con numeros (del 1 al 7), el dia de la semama: ";cin>>dia;
+
+    switch (dia) {
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        case 6: cout<<"Ese dia es laboral pa, a laburar :)"; break;
+        case 7: cout<<"Ese dia no es laboral pa, a descansar....\npara seguir trabajando xd :)"; break;
+        default: cout<<"Que pusiste xd?"; break;
+
+        
+    }
+    
+    
+    
+    
+      
+    return 0;
+}
+*/
